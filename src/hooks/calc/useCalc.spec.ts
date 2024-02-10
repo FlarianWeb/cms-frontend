3ee8d72bfb9617eca2181f925c00ff08b1f3339e
MusinRamil <musin@flarian.ru>
2024-02-10 05:09:25 +0300

    test bad test

--- src/hooks/calc/useCalc.spec.ts
+++ src/hooks/calc/useCalc.spec.ts
@@ -27,8 +27,8 @@ describe('useCalc', () => {
 			expect(() => minus(1)).toThrowError(/^Parameter «B» is undefined!$/);
 		});
 
-		test('minus(3, 1) to equal 4', () => {
-			expect(minus(3, 1)).toEqual(2);
+		test('minus(3, 1) to equal 2', () => {
+			expect(minus(3, 1)).toEqual(3);
 		});
 	});
 });

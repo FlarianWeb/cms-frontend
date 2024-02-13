30ec6718cb4db715c56a0e5b093a26e61be60ccf
MusinRamil <musin@flarian.ru>
2024-02-13 13:39:43 +0300

    CMS-9_common_types

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,8 +4,8 @@
  * @template T - Тип enum.
  * @param {T} obj - Объект enum.
  */
-export const enumKeys = <T extends Record<string, unknown>>(obj: T) =>
-	Object.keys(obj).filter(v => isNaN(Number(v))) as Array<keyof typeof obj>;
+export const enumKeys = <T extends Record<string, unknown>>(obj: T): Array<keyof typeof obj> =>
+	Object.keys(obj).filter(v => isNaN(Number(v)));
 
 /**
  * Возвращает массив значений enum с соответствующим типом.
@@ -13,6 +13,5 @@ export const enumKeys = <T extends Record<string, unknown>>(obj: T) =>
  * @template T - Тип enum.
  * @param {T} obj - Объект enum.
  */
-export const enumValues = <T extends Record<string, unknown>>(obj: T) => {
-	return Object.values(obj).filter(v => isNaN(Number(v))) as Array<T[keyof T]>;
-};
+export const enumValues = <T extends Record<string, unknown>>(obj: T): Array<unknown> =>
+	Object.values(obj).filter(v => isNaN(Number(v)));
